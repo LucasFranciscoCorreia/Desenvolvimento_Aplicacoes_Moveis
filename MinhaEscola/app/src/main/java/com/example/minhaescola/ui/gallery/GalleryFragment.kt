5ad97f468dbe5c31c134e0da39875019ff8f2c22
Bartô Alheiros <alheirosb@gmail.com>
2019-10-21 13:38:43 -0300

    Menu princiapal dado início. Ícone de "busque a sua escola" adicionado.

--- MinhaEscola/app/src/main/java/com/example/minhaescola/ui/gallery/GalleryFragment.kt
+++ MinhaEscola/app/src/main/java/com/example/minhaescola/ui/gallery/GalleryFragment.kt
@@ -16,9 +16,7 @@ class GalleryFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         galleryViewModel = ViewModelProviders.of(this).get(GalleryViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        galleryViewModel.text.observe(this, Observer { textView.text = it })
-        return root
+        return inflater.inflate(R.layout.fragment_gallery, container, false)
+        // galleryViewModel.text.observe(this, Observer { textView.text = it })
     }
 }
