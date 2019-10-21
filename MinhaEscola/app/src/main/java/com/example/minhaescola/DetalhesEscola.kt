01191e8fd63661f62a06d44910b6b663d237be86
Bartô Alheiros <alheirosb@gmail.com>
2019-10-21 19:40:07 -0300

    Botão avalie(Tela Detalhes) abrindo tela de avaliação.

--- MinhaEscola/app/src/main/java/com/example/minhaescola/DetalhesEscola.kt
+++ MinhaEscola/app/src/main/java/com/example/minhaescola/DetalhesEscola.kt
@@ -2,11 +2,22 @@ package com.example.minhaescola
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.view.View
+
 
 class DetalhesEscola : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_detalhes_escola)
+    }
+
+    fun avaliar(view:View?) {
+        val i = Intent(this, activity_avaliacao::class.java)
+        startActivity(i)
     }
 }
---
+++ MinhaEscola/app/src/main/java/com/example/minhaescola/activity_avaliacao.kt
@@ -0,0 +1,12 @@
+package com.example.minhaescola
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_avaliacao : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_avaliacao)
+    }
+}
