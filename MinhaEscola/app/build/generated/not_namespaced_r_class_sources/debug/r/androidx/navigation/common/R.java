579001eb7475af5407f52278a8a84c35a0d2d225
Bartô Alheiros <alheirosb@gmail.com>
2019-11-21 21:37:35 -0300

    Progamando o searchview da toolbar.

--- MinhaEscola/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/navigation/common/R.java
+++ MinhaEscola/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/navigation/common/R.java
@@ -137,7 +137,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f11005b;
+        public static final int status_bar_notification_info_overflow = 0x7f11005c;
     }
     public static final class style {
         private style() {}
--- MinhaEscola/app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/firebase/database/R.java
+++ MinhaEscola/app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/firebase/database/R.java
@@ -206,7 +206,7 @@ public final class R {
         public static final int common_open_on_phone = 0x7f110033;
         public static final int common_signin_button_text = 0x7f110034;
         public static final int common_signin_button_text_long = 0x7f110035;
-        public static final int status_bar_notification_info_overflow = 0x7f11005b;
+        public static final int status_bar_notification_info_overflow = 0x7f11005c;
     }
     public static final class style {
         private style() {}
